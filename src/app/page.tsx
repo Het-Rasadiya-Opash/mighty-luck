755a61b72bb0d21d737841c149ae781538b1233e
Het-Rasadiya-Opash <hra.opash@gmail.com>
2026-06-11 17:59:54 +0530

    feat: add bonus buy and table game sections to landing page with associated assets and data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Sidebar from "@/components/layout/Sidebar";
 import HeroBanner from "@/components/sections/HeroBanner";
 import HeroSection1 from "@/components/sections/HeroSection1";
 import GameSlider from "@/components/sections/GameSlider";
-import { Cherry, Zap, Rocket } from "lucide-react";
+import { Cherry, Zap, Rocket, Dices, CircleDollarSign } from "lucide-react";
 import slotsData from "@/data/slotsData.json";
 import originalsData from "@/data/originalsData.json";
 import crashGamesData from "@/data/crashGamesData.json";
+import tableGamesData from "@/data/tableGamesData.json";
+import bonusBuysData from "@/data/bonusBuysData.json";
 import WhyJoin from "@/components/sections/WhyJoin";
 import ProviderSection from "@/components/sections/ProviderSection";
 
@@ -18,28 +20,40 @@ export default function Home() {
         <main className="w-[1136px] h-[2644px] flex flex-col gap-[40px]">
           <HeroBanner />
           <HeroSection1 />
-          
-          <GameSlider 
-            title="SLOTS (1,487)" 
-            icon={<Cherry className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} />} 
-            games={slotsData} 
-          />
-          
-          <GameSlider 
-            title="ORIGINALS (14)" 
-            icon={<Zap className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />} 
-            games={originalsData} 
+
+          <GameSlider
+            title="SLOTS (1,487)"
+            icon={<Cherry className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} />}
+            games={slotsData}
           />
 
-          <WhyJoin/>
+          <GameSlider
+            title="ORIGINALS (14)"
+            icon={<Zap className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />}
+            games={originalsData}
+          />
 
-          <GameSlider 
-            title="CRASH GAMES (723)" 
-            icon={<Rocket className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />} 
-            games={crashGamesData} 
+          <WhyJoin />
+
+          <GameSlider
+            title="CRASH GAMES (723)"
+            icon={<Rocket className="text-[#FFBF1F] w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />}
+            games={crashGamesData}
           />
 
           <ProviderSection />
+
+          <GameSlider
+            title="TABLE GAMES (51)"
+            icon={<Dices className="text-black w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />}
+            games={tableGamesData}
+          />
+
+           <GameSlider
+            title="BONUS BUYS (145)"
+            icon={<CircleDollarSign className="text-black w-[24px] h-[24px] shrink-0" strokeWidth={2} fill="#FFBF1F" />}
+            games={bonusBuysData}
+          />
         </main>
       </div>
     </Container>
