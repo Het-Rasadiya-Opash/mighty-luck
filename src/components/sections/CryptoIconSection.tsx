b8b02c639252c280ad77edc02a10739e1e7ce5a0
Het-Rasadiya-Opash <hra.opash@gmail.com>
2026-06-17 12:36:47 +0530

    feat: add CryptoIconSection component to display a collection of cryptocurrency icons

--- src/components/sections/CryptoIconSection.tsx
+++ src/components/sections/CryptoIconSection.tsx
@@ -5,7 +5,8 @@ const icons = ['c1.svg', 'c2.svg', 'c3.svg', 'c4.svg', 'c5.svg', 'c6.svg', 'c7.s
 
 export default function CryptoIconSection() {
     return (
-        <section className="relative w-full border-b border-[#112F82] px-4 md:px-[40px] py-4 md:py-0 md:h-[100px] flex items-center justify-center overflow-hidden isolate">
+        <section className="relative w-full md:w-full h-[100px] border-b border-[#112F82] px-[20px] md:px-[40px] flex flex-row items-center justify-between md:justify-center gap-[79px] overflow-hidden isolate box-border">
+            {/* Ellipse 6 */}
             <div
                 className="absolute w-[390px] h-[390px] bg-[#1463FF] pointer-events-none rounded-full"
                 style={{
@@ -15,9 +16,10 @@ export default function CryptoIconSection() {
                     zIndex: 0
                 }}
             />
-            <div className="flex flex-wrap items-center justify-center gap-4 md:gap-[28px] relative z-[1]">
+            {/* Icons Frame */}
+            <div className="flex flex-row flex-wrap items-center justify-center content-center gap-x-[31px] gap-y-[16px] md:gap-[28px] w-[334px] md:w-auto h-[53.61px] md:h-auto mx-auto relative z-[1]">
                 {icons.map((icon, i) => (
-                    <div key={i} className="flex items-center justify-center transition-opacity cursor-pointer min-h-[44px] min-w-[44px] md:min-h-0 md:min-w-0">
+                    <div key={i} className="flex items-center justify-center transition-opacity cursor-pointer flex-none">
                         <Image src={`/${icon}`} alt={`Crypto ${i + 1}`} width={19} height={19} className="w-auto h-[16px] md:h-[19px] shrink-0 object-contain" />
                     </div>
                 ))}
@@ -25,3 +27,4 @@ export default function CryptoIconSection() {
         </section>
     );
 }
+
