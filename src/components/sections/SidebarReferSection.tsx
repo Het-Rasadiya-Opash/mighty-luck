2f51f6fc0ab9b64c6497ef0a844b8e0f724f70ba
Het-Rasadiya-Opash <hra.opash@gmail.com>
2026-06-23 11:54:41 +0530

    feat: add SidebarReferSection component with promotional referral banners

--- src/components/sections/SidebarReferSection.tsx
+++ src/components/sections/SidebarReferSection.tsx
@@ -2,31 +2,29 @@
 
 export default function SidebarReferSection() {
     return (
-        <div className="flex flex-col gap-[8px] w-full flex-none">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-[8px] flex-none">
 
-            <div className="flex flex-row gap-[8px] w-full">
-                <div className="flex-1 rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity">
-                    <img
-                        src="/raf.svg"
-                        alt="Refer a Friend"
-                        className="w-full h-auto block"
-                    />
-                </div>
-
-                <div className="flex-1 rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity">
-                    <img
-                        src="/vt.svg"
-                        alt="VIP Transfer"
-                        className="w-full h-auto block"
-                    />
-                </div>
+            <div className="col-span-1 rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity sm:flex">
+                <img
+                    src="/raf.svg"
+                    alt="Refer a Friend"
+                    className="w-full h-auto sm:h-full sm:object-cover block"
+                />
             </div>
 
-            <div className="w-full rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity">
+            <div className="col-span-1 rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity sm:flex">
+                <img
+                    src="/vt.svg"
+                    alt="VIP Transfer"
+                    className="w-full h-auto sm:h-full sm:object-cover block"
+                />
+            </div>
+
+            <div className="col-span-2 rounded-[10px] overflow-hidden cursor-pointer hover:opacity-90 transition-opacity sm:flex">
                 <img
                     src="/wr.svg"
                     alt="Winter Rush"
-                    className="w-full h-auto block"
+                    className="w-full h-auto sm:h-full sm:object-cover block"
                 />
             </div>
 
