db5a744ccca66354bf183092a4133926bd24d9ee
Het-Rasadiya-Opash <hra.opash@gmail.com>
2026-06-25 13:17:56 +0530

    feat: add AboutSection, TabSection, Container, and Header components to the UI layout

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 export default function Container({ children }: ContainerProps) {
   return (
     <main className="w-full bg-[#091741] flex justify-center flex-1 overflow-x-hidden">
-      <div className="w-full pb-[40px] px-4 sm:px-6 md:px-8 lg:px-12 xl:px-6 pt-4 md:pt-6">
+      <div className="w-full max-w-[1696px] pb-[40px] px-4 sm:px-6 md:px-8 lg:px-12 xl:px-6 pt-4 md:pt-6">
         {children}
       </div>
     </main>
